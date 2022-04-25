dbe07c813864d70c063d408c6a5b0f8248cdf1d8
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2022-04-25 11:32:32 -0700

    Bumping to version 0.3.2

--- Formula/Marsrawutils.rb
+++ Formula/Marsrawutils.rb
@@ -4,9 +4,9 @@
 class Marsrawutils < Formula
     desc "Utilities for working with publicly available raw MSL, Mars2020, and InSight images"
     homepage "https://github.com/kmgill/mars-raw-utils"
-    url "https://github.com/kmgill/mars-raw-utils/archive/refs/tags/v0.3.0.tar.gz"
+    url "https://github.com/kmgill/mars-raw-utils/archive/refs/tags/v0.3.2.tar.gz"
     sha256 "ae6c651b518f151988368478efb8e6653a00ed7f47d35f4355b6962f0cf1b06e"
-    version "0.3.0"
+    version "0.3.2"
 
     depends_on "rust" => :build
 
