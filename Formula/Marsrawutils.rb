9fda35c5592d666ecd48cac66fb035f97d80658d
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2022-04-25 11:35:02 -0700

    Gotta update the sha256 hash. Gotta lot to learn here :-)

--- Formula/Marsrawutils.rb
+++ Formula/Marsrawutils.rb
@@ -5,7 +5,7 @@ class Marsrawutils < Formula
     desc "Utilities for working with publicly available raw MSL, Mars2020, and InSight images"
     homepage "https://github.com/kmgill/mars-raw-utils"
     url "https://github.com/kmgill/mars-raw-utils/archive/refs/tags/v0.3.2.tar.gz"
-    sha256 "ae6c651b518f151988368478efb8e6653a00ed7f47d35f4355b6962f0cf1b06e"
+    sha256 "30e4c279118a954de69a2e7366be7d0d18d3a7af8b81d8922559dd342c073ed7"
     version "0.3.2"
 
     depends_on "rust" => :build
