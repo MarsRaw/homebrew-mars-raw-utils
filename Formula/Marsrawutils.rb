1f6ee4ec181bbd47a1d6c710e48016ee07648c52
Kevin M. Gill <kmsmgill@gmail.com>
2022-05-08 14:09:15 -0700

    Updating for MRU 0.4.0

--- Formula/Marsrawutils.rb
+++ Formula/Marsrawutils.rb
@@ -4,9 +4,9 @@
 class Marsrawutils < Formula
     desc "Utilities for working with publicly available raw MSL, Mars2020, and InSight images"
     homepage "https://github.com/kmgill/mars-raw-utils"
-    url "https://github.com/kmgill/mars-raw-utils/archive/refs/tags/v0.3.4.tar.gz"
-    sha256 "3e02bf0508808cde02c2322d834fd3f590afd4bc411729112792daccd76937a8"
-    version "0.3.4"
+    url "https://github.com/kmgill/mars-raw-utils/archive/refs/tags/v0.4.0.tar.gz"
+    sha256 "ba7e3d07aeb0da65364a72fdc52e2c5c3261e004039acf18d2843db1315f7549"
+    version "0.4.0"
 
     depends_on "rust" => :build
 
